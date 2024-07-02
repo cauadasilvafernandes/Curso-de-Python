44a2142de9e0e6870ae9d5486f6399cccb7d02f1
Cauã da Silva Fernandes <cfernandes.dev@gmail.com>
2024-07-02 19:56:15 -0300

    Curso em vídeo
    Fazendo os exercícios em Python

---
+++ Exercícios/Soma/Dissecando-variavel/Antecessor-Sucessor/index.py
@@ -0,0 +1,7 @@
+entrada = int(input("Digite um número:"))
+
+antecessor = entrada - 1
+sucessor = entrada + 1
+
+print(f"O valor {entrada}, seu antecessor é {antecessor} e o sucessor é {sucessor}")
+        

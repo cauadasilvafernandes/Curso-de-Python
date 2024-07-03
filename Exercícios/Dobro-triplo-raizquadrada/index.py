169e7b0cad4c126bb6a6764c96efae2e238adddf
Cauã da Silva Fernandes <cfernandes.dev@gmail.com>
2024-07-02 23:03:41 -0300

    Dobro-triplo-raiz
    Fiz mais uma atividade do curso em vídeo.

---
+++ Exercícios/Dobro-triplo-raizquadrada/index.py
@@ -0,0 +1,8 @@
+a = int(input("Digite um número:"))
+
+dobro = a * 2 
+triplo = a * 3
+raiz = a ** (1/2)
+print(f"O dobro de {a} vale: {dobro}")
+print(f"O triplo de {a} vale : {triplo}")
+print (f"A raiz quadrada de {a} vale: {raiz :.2f}")

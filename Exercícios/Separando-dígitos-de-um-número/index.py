39bf322b60e21836d22be70d13600ce517e4faf1
Cauã da Silva Fernandes <cfernandes.dev@gmail.com>
2024-07-10 08:36:54 -0300

    Refazendo para ter o entendimento do conteudo.

--- Exercícios/Separando-dígitos-de-um-número/index.py
+++ Exercícios/Separando-dígitos-de-um-número/index.py
@@ -1,12 +1,16 @@
-num = int(input("Informe um número:"))
+# Faça um programa que leia um número
+#  de 0 a 9999
+#  e mostre na tela cada um dos dígitos separados.
 
-u = num // 1 % 10 
-d = num // 10 % 10
-c = num // 100 % 10 
-m = num // 1000 % 10
+numero = int(input("Digite um número:"))
 
-print(f"Analisando o número {num}")
-print(f" Unidade {u}")
-print(f" Dezena  {d}")
-print(f" Centena {c}")
-print(f" Milhar  {m}")
+u = numero // 1 % 10
+d = numero // 10 % 10
+c = numero // 100 % 10
+m = numero // 1000 % 10
+
+print(f"Unidade: {u}")
+print(f"Dezena: {d}")
+print(f"Centena: {c}")
+print(f"Milhar: {m}")
+

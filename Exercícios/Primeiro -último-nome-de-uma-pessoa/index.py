eeb4f1d46a0e6330621ba612211948934176dd93
Cauã da Silva Fernandes <cfernandes.dev@gmail.com>
2024-07-11 19:19:13 -0300

    Primeiro e último nome de uma pessoa

---
+++ Exercícios/Primeiro -último-nome-de-uma-pessoa/index.py
@@ -0,0 +1,7 @@
+n = str(input("Digite o seu nome:")).strip()
+
+nome = n.split()
+
+print("Prazer em te conhecer!")
+print(f"Seu primeiro nome é: {nome[0]}")
+print(f"Seu último nome é: {nome[len(nome)-1]}")

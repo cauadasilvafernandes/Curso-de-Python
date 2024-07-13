847d439e994f06ed5db099ea9ed7fe1d3e852d96
Cauã da Silva Fernandes <cfernandes.dev@gmail.com>
2024-07-13 12:31:04 -0300

    exercício de adivinhação

---
+++ Exercícios/Jogo-da-Adivinhação/index.py
@@ -0,0 +1,18 @@
+from time import sleep
+import random
+
+computador = random.randint(0, 10 )
+
+print('-=-' * 20)
+print("Vou pensar em um número 0 e 5. Tente adivinhar.")
+print('-=-' * 20)
+
+jogador = int(input("Em que número eu pensei?"))
+
+print("Processando...")
+sleep(3)
+
+if jogador == computador:
+    print("PARABÉNS!")
+else:
+    print(f"GANHEI! Eu pensei no número {computador} e não no {jogador}!")
